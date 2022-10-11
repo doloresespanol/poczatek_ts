0917759cd5c71fe847e6b17ec2c8e0301c2d9c13
doloresespanol <popokaja9@gmail.com>
2022-10-11 09:49:46 +0200

    klasy_ts

--- restauracja.js
+++ restauracja.js
@@ -3,11 +3,20 @@ var Restauracja = /** @class */ (function () {
         this.pizzaInOrder = [];
         this.maxPizzasInOven = 10;
         this.Recipies = [];
+        this._manager = "Jan";
         this.name = name;
+        this.id = Restauracja.id++;
     }
-    Restauracja.prototype.order = function (pizza) {
-        this.pizzaInOrder.push(pizza);
-    };
+    Object.defineProperty(Restauracja.prototype, "manager", {
+        get: function () {
+            return this._manager;
+        },
+        set: function (manager) {
+            this._manager = manager;
+        },
+        enumerable: false,
+        configurable: true
+    });
     Restauracja.prototype.isOvenFull = function () {
         if (this.pizzaInOrder.length <= 10) {
             return true;
@@ -16,9 +25,26 @@ var Restauracja = /** @class */ (function () {
             return false;
         }
     };
+    Restauracja.prototype.order = function (pizza) {
+        if (this.isOvenFull() == true) {
+            console.log("Piec pelny");
+        }
+        else {
+            this.pizzaInOrder.push(pizza);
+        }
+    };
+    Restauracja.id = 0;
     return Restauracja;
 }());
-var dagrasso = new Restauracja("dagrasso");
-dagrasso.order("Wiejska");
+var dagrasso = new Restauracja('Dagrasso');
+dagrasso.order("hawajska");
 console.log(dagrasso.name);
+dagrasso.manager = "Ola";
+console.log(dagrasso.manager);
+var pizzahut = new Restauracja("PizzaHut");
+var dominium = new Restauracja("Dominium");
+var rome = new Restauracja("Rome");
+console.log(dominium);
+console.log(rome.id);
+console.log(pizzahut.id);
 //dagrasso.isOvenFull();
--- restauracja.ts
+++ restauracja.ts
@@ -1,16 +1,24 @@
 class Restauracja{
+    static id: number = 0;
     readonly name:string;
     private pizzaInOrder:string[]=[];
     private maxPizzasInOven:number = 10;
     protected Recipies:string[] = [];
+    private _manager: string = "Jan";
 
     constructor(name:string){
         this.name=name;
+        this.id=Restauracja.id++;
     }
 
-    public order(pizza:string):void{
-        this.pizzaInOrder.push(pizza);
+    get manager(){
+        return this._manager;
     }
+
+    set manager(manager){
+        this._manager = manager;
+    }
+
     private isOvenFull():boolean{
         if(this.pizzaInOrder.length<=10){
             return true;
@@ -18,9 +26,27 @@ class Restauracja{
             return false;
         }
     }
+
+    public order(pizza:string):void{
+        if(this.isOvenFull()==true){
+            console.log("Piec pelny");
+        }else{
+            this.pizzaInOrder.push(pizza);
+        }
+    }
 }
 
-const dagrasso : Restauracja=new Restauracja("dagrasso");
-dagrasso.order("Wiejska");
+const dagrasso: Restauracja = new Restauracja('Dagrasso');
+dagrasso.order("hawajska");
 console.log(dagrasso.name);
+dagrasso.manager = "Ola";
+console.log(dagrasso.manager);
+ 
+const pizzahut: Restauracja = new Restauracja("PizzaHut");
+const dominium: Restauracja = new Restauracja("Dominium");
+const rome: Restauracja = new Restauracja("Rome");
+ 
+console.log(dominium);
+console.log(rome.id);
+console.log(pizzahut.id);
 //dagrasso.isOvenFull();
